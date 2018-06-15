30a5b6b58cb332e3b4ca7aa1d353e633153bca92
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2018-06-15 11:31:46 +0800

    init

---
+++ SIMSTxtData.m
@@ -0,0 +1,82 @@
+classdef SIMSTxtData
+    
+    properties(SetAccess = private)
+        sampleName;
+        mz;
+        scanLength;
+        shotsPerPixel;
+        imageSize;
+        rawMat;
+    end
+    
+    methods
+        function obj = SIMSTxtData(fpath)
+            if nargin == 0
+                [fn,fp] = uigetfile('*.txt');
+                fpath = strcat(fp,fn);
+            end
+            try
+                fid = fopen(fpath);
+                curStr = fgetl(fid);
+                while ~isempty(curStr) && curStr(1)=='#'
+                    if contains(curStr,'Source Filename:')
+                        m = regexp(curStr,'Source Filename: (?<name>\S+)','names');
+                        obj.sampleName = m.name;
+                    elseif contains(curStr,'Source Interval:')
+                        m = regexp(curStr,'Source Interval: (?<name>\S+) \S+','names');
+                        if strcmp(m.name,'total')
+                            obj.mz = -1;
+                        else
+                            obj.mz = str2double(m.name);
+                        end
+                    elseif contains(curStr,'Field of View')
+                        m = regexp(curStr,'Field of View: (?<name>\S+) \S+','names');
+                        obj.scanLength = str2double(m.name);
+                    elseif contains(curStr,'Shots')
+                        m = regexp(curStr,'Shots per pixel: (?<name>\S+)','names');
+                        obj.shotsPerPixel = str2double(m.name);
+                    elseif contains(curStr,'Image Size')
+                        m = regexp(curStr,'Image Size: (?<name>\S+) \S+','names');
+                        obj.imageSize = str2double(m.name);
+                    end
+                    curStr = fgetl(fid);
+                end
+                obj.rawMat = zeros(obj.imageSize);
+                curPoint = 1;
+                while ~isnumeric(curStr)
+                    try
+                        %m = str2double(strsplit(curStr,' '));
+                        %obj.rawMat(curPoint) = m(3);
+                        
+                        %m = strsplit(curStr,' ');
+                        %obj.rawMat(curPoint) = str2double(m{3});
+                        
+                        m = regexp(curStr,'\S+ \S+ (?<name>\S+)','names');
+                        obj.rawMat(curPoint) = str2double(m.name);
+                        
+                        curPoint = curPoint + 1;
+                    catch
+                    end
+                    curStr = fgetl(fid);
+                end
+                if curPoint ~= (obj.imageSize^2 + 1)
+                    warning('Parsing number not much!');
+                end
+            catch e
+                close(fid);
+                disp(e);
+            end
+        end
+        function show(obj,varargin)
+            imagesc(obj.rawMat); 
+            if nargin == 1
+                colormap('hot');
+            else
+                colormap(varargin{1});
+            end
+            title(sprintf('%s - m/z: %.2f',obj.sampleName,obj.mz),'Interpreter','none');
+        end
+    end
+    
+end
+
